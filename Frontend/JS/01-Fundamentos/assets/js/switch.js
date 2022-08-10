77ffcdd447e09db4d50ec74db361dde8ca01da29
Laura Daniela Gonzalez Alvarez <lhaogonzaleza@gmail.com>
2022-08-10 00:42:49 -1000

    switch.js

---
+++ Frontend/JS/01-Fundamentos/assets/js/switch.js
@@ -0,0 +1,15 @@
+const dia  = 2; //
+
+switch (dia) {
+    case 0:
+        console.log('Domingo');
+        break;
+    case 1:
+        console.log('Lunes');
+        break;
+    case 2:
+        console.log('Martes');
+        break;
+    default:
+            console.log('No es un día válido');
+}
