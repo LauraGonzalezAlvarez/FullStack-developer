e492a224e6efb9b5a60248353a22b13d45e40a96
Laura Daniela Gonzalez Alvarez <lhaogonzaleza@gmail.com>
2022-08-21 18:15:26 -0500

    Api coompleta

--- Frontend/JS/sofka-test-HTML/js/formulario.js
+++ Frontend/JS/sofka-test-HTML/js/formulario.js
@@ -1,8 +1,3 @@
-const cedulita = document.getElementById('cedula'); // creado para validar el campo de cedula
-
-
-
-
 const formulario = document.getElementById('formulario');
 const inputs = document.querySelectorAll('#formulario input');
 const emailRegex = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
@@ -17,59 +12,56 @@ const campos = {
 const validarFormulario = (e) => {
     switch (e.target.name) {
 
-       case "nombre":
-           validarCampoNombres(e.target, 'nombre');
-          break;
+        case "nombre":
+            validarCampoNombres(e.target, 'nombre');
+            break;
         case "cedula":
-            validarCampoCedula( e.target, 'cedula');
+            validarCampoCedula(e.target, 'cedula');
             break;
-
         case "correo":
-            validarCampoEmail( e.target, 'correo');
-           break;
+            validarCampoEmail(e.target, 'correo');
+            break;
         case "telefono":
-            validarCampoTelefono( e.target, 'telefono');
+            validarCampoTelefono(e.target, 'telefono');
+            break;
+        case "operacion":
+            validarCampoOperacion(e.target, 'operacion');
             break;
     }
 }
 
-const validarCampoCedula = ( input, campo) => {
-    if(input.value > 1000000000 ){
-        validarFormularioCorrecto(input, campo);
-
-    } else {
-        validarFormularioIncorrecto(input, campo);
-
-    }
-}
-
-const validarCampoEmail = ( input, campo) => {
-    if(emailRegex.test(input.value)){
+const validarCampoCedula = (input, campo) => {
+    if (input.value > 1000000000) {
         validarFormularioCorrecto(input, campo);
     } else {
         validarFormularioIncorrecto(input, campo);
     }
 }
 
+const validarCampoEmail = (input, campo) => {
+    if (emailRegex.test(input.value)) {
+        validarFormularioCorrecto(input, campo);
+    } else {
+        validarFormularioIncorrecto(input, campo);
+    }
+}
 
-
-const validarCampoTelefono = ( input, campo) => {
-  if(input.value.charAt(0) === '3' && input.value.charAt(9)){
-      validarFormularioCorrecto(input, campo);
-  } else {
-      validarFormularioIncorrecto(input, campo);
-  }
+const validarCampoTelefono = (input, campo) => {
+    if (input.value.charAt(0) === '3' && input.value.charAt(9)) {
+        validarFormularioCorrecto(input, campo);
+    } else {
+        validarFormularioIncorrecto(input, campo);
+    }
 }
 
 
-const validarCampoNombres = ( input, campo) => {
+const validarCampoNombres = (input, campo) => {
     const myArray = input.value.split(" ");
     let bandera = false;
     for (let myArrayElement of myArray) {
         (myArrayElement === myArrayElement.charAt(0).toUpperCase() + myArrayElement.slice(1)) ? bandera = false : bandera = true;
     }
-
-    (myArray[2] && bandera===false) ? validarFormularioCorrecto(input, campo) : validarFormularioIncorrecto(input, campo);
+    (myArray[2] && bandera === false) ? validarFormularioCorrecto(input, campo) : validarFormularioIncorrecto(input, campo);
 }
 
 const validarFormularioCorrecto = (input, campo) => {
@@ -99,9 +91,7 @@ inputs.forEach((input) => {
 formulario.addEventListener('submit', (e) => {
     e.preventDefault();
 
-
-
-    if( campos.nombre && campos.cedula && campos.correo && campos.telefono  ){
+    if (campos.nombre && campos.cedula && campos.correo && campos.telefono && campos.operacion) {
         formulario.reset();
 
         document.getElementById('formulario__mensaje-exito').classList.add('formulario__mensaje-exito-activo');
@@ -113,21 +103,30 @@ formulario.addEventListener('submit', (e) => {
             icono.classList.remove('formulario__grupo-correcto');
         });
     } else {
-        document.getElementById('formulario__mensaje').classList.add('formulario__mensaje-activo');
+        document.getElementById('formulario__mensaje-error').classList.add('formulario__mensaje-activo');
+        setTimeout(() => {
+            document.getElementById('formulario__mensaje-error').classList.remove('formulario__mensaje-activo');
+        }, 3000);
     }
 });
 
-const options = {
-    method: 'POST',
-    headers: {
-        'content-type': 'application/json',
-        'X-RapidAPI-Key': 'cbf4e25799msh107022995e13f3dp1da248jsn31b6a2ca4352',
-        'X-RapidAPI-Host': 'math6.p.rapidapi.com'
-    },
-    body: '{"data":"2 + 2"}'
-};
 
-fetch('https://math6.p.rapidapi.com/generate', options)
-    .then(response => response.json())
-    .then(response => console.log(response))
-    .catch(err => console.error(err));
+
+validarCampoOperacion = (input, campo) => {
+    let buton = document.getElementById('button')
+    buton.addEventListener('click', () => {
+        (input.value) ? validarFormularioCorrecto(input, campo) : validarFormularioIncorrecto(input, campo);
+        let valorOperacion = document.getElementById('operacion').value
+        const url = `http://api.mathjs.org/v4/?expr=2%2B3*sqrt(${valorOperacion})`;
+
+        fetch(url)
+            .then(response => response.json())
+            .then(data => {
+                let resul = document.getElementById('resultado');
+                resul.innerHTML = `<p>${data}</p>`;
+            })
+    })
+}
+
+
+
