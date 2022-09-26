0fd7530785f89bffd19670dcd146ccb4f03f83df
Laura Daniela Gonzalez Alvarez <lhaogonzaleza@gmail.com>
2022-09-26 10:17:28 -0500

    Clases en java

--- Frontend/JS/01-Fundamentos/assets/js/classes/class.js
+++ Frontend/JS/01-Fundamentos/assets/js/classes/class.js
@@ -5,12 +5,21 @@ class PersonaNatural{
 
     constructor(nombre='sin nombre', codigo='son codigo', frase='sin frase'){
        // console.log('Hola');
-        this.codigo= 'codigo';
+        this.codigo= 'código';
         this.nombre= 'nombre';
         this.frase= 'frase';
 
+    }
+    quienSoy(){
+        console.log(`Soy ${this.nombre} y mi identidad es ${this.codigo}`);
+    }
+    miFrase(){
+        this.quienSoy();
+        console.log(`${this.codigo} dice: ${this.frase}`);
     }
 }
 
 const spiderman = new PersonaNatural('Peter Parker', 'Spiderman', 'Soy tu amigable vecino Spiderman');
 console.log(spiderman);
+spiderman.quienSoy();
+spiderman.miFrase();
