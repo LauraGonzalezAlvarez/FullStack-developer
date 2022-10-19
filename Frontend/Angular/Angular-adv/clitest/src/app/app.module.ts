0e224a924a9ea44f1df26c7979da5f5cdcb5f179
Laura Daniela Gonzalez Alvarez <lhaogonzaleza@gmail.com>
2022-10-19 16:22:28 -0500

    Clase 1

---
+++ Frontend/Angular/Angular-adv/clitest/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    AboutComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
