099b51a05e98cb43a69b75507399f57df64bd2bb
Laura Daniela Gonzalez Alvarez <lhaogonzaleza@gmail.com>
2022-10-20 20:55:31 -0500

    Trabajo manejo de errores

--- Frontend/Angular/Angular-adv/clitest/src/app/app.module.ts
+++ Frontend/Angular/Angular-adv/clitest/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
+import {ContactComponent} from "./pages/contact/contact.component";
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    AboutComponent
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule
--- Frontend/Angular/Angular-adv/clitest/src/app/pages/home/home.component.ts
+++ Frontend/Angular/Angular-adv/clitest/src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {AuthService} from "../../services/auth.service";
 
 @Component({
   selector: 'app-home',
@@ -6,9 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent  {
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit(): void {
   }
