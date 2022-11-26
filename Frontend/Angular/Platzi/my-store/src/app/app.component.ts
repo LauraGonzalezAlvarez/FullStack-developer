d9c29f7fe2214d6681e9993f94fbba0f6189a4b8
Laura Daniela Gonzalez Alvarez <lhaogonzaleza@gmail.com>
2022-11-25 19:05:44 -0500

    Notaso clase 9 de fundamentos de angular

--- Frontend/Angular/Platzi/my-store/src/app/app.component.ts
+++ Frontend/Angular/Platzi/my-store/src/app/app.component.ts
@@ -5,7 +5,7 @@ import { Product } from './product.model';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   //Atributos de la clase
@@ -20,20 +20,20 @@ export class AppComponent {
     name: '',
     email: '',
     password: '',
-  }
+  };
 
   //Ejemplo para renderizar un objeto
   person = {
     name: 'Nicolas',
     age: 18,
-    avatar: 'https://www.w3schools.com/howto/img_avatar.png'
-  }
+    avatar: 'https://www.w3schools.com/howto/img_avatar.png',
+  };
   names: string[] = ['Nico', 'Juli', 'Santi'];
   newName = '';
   box = {
     width: 100,
     height: 100,
-    background: 'red'
+    background: 'red',
   };
   // El array de products es de tipo Product, que es una interface
   products: Product[] = [
@@ -46,36 +46,35 @@ export class AppComponent {
     {
       name: 'Bicicleta casi nueva',
       price: 356,
-      image: './assets/images/bike.jpg'
+      image: './assets/images/bike.jpg',
     },
     {
       name: 'Colleción de albumnes',
       price: 34,
-      image: './assets/images/album.jpg'
+      image: './assets/images/album.jpg',
     },
     {
       name: 'Mis libros',
       price: 23,
-      image: './assets/images/books.jpg'
+      image: './assets/images/books.jpg',
     },
     {
       name: 'Casa para perro',
       price: 34,
-      image: './assets/images/house.jpg'
+      image: './assets/images/house.jpg',
     },
     {
       name: 'Gafas',
       price: 3434,
-      image: './assets/images/glasses.jpg'
-    }
-  ]
+      image: './assets/images/glasses.jpg',
+    },
+  ];
 
   //Metodos
   toggleButton() {
     this.btnDisabled = !this.btnDisabled;
-
   }
- // Metododos
+  // Metododos
   increaseAge() {
     this.person.age += 1;
   }
@@ -83,17 +82,18 @@ export class AppComponent {
   // Metodo para detectar el evento de click
   onScroll(event: Event) {
     const element = event.target as HTMLElement;
-    // HTMLElement = Esta interface representa cualquier elemento HTML, algunos elementos implementan ctamente esta interface , mientras que otros la implementan a través de una interfaz que hereda de esta.
+    // HTMLElement = Esta interface representa cualquier elemento HTML, algunos elementos implementan esta interface , mientras que otros la implementan a través de una interfaz que hereda de esta.
     console.log(element.scrollTop);
   }
   //La EventTargetinterfaz está implementada por objetos que pueden recibir eventos y pueden tener oyentes para ellos. En otras palabras, cualquier objetivo de eventos implementa los tres métodos asociados con esta interfaz.
   //
   // Element, y sus hijos, así como Documenty Window, son los destinos de eventos más comunes, pero otros objetos también pueden ser destinos de eventos. Por ejemplo XMLHttpRequest, AudioNodey AudioContexttambién son destinos de eventos.
+  // Necesitamos el target es decir el elemento HTML as HTMLElement = como un elemento html ese sería el tipado (Lo que dice el profesor de platzi)
 
   changeName(event: Event) {
-    const element = event.target as HTMLInputElement;
+    const element = event.target as HTMLInputElement; // HTMLInputElement = Estamos leyendo un input
     // HTMLInputElement = Esta interface proporciona  propiedades y métodos para manipular elementos de entrada de tipo <input> en un documento HTML.
-    this.person.name = element.value;
+    this.person.name = element.value; // value = es el valor que tiene el input y lo guardo por el valor que tiene el input
   }
 
   addName() {
