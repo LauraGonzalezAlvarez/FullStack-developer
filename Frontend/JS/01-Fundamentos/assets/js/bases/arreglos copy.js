f12bcbdb6a87b9fbb28884993a315dc3e918739a
Laura Daniela Gonzalez Alvarez <lhaogonzaleza@gmail.com>
2023-01-04 22:45:49 -0500

    Repaso JS

---
+++ Frontend/JS/01-Fundamentos/assets/js/bases/arreglos copy.js
@@ -0,0 +1,22 @@
+const arr = new Array(10);
+console.log(arr);
+
+const arr2 = [];
+console.log(arr2);
+
+
+let videoJuegos = ['Mario 3', 'Megaman'];
+console.log({videoJuegos});
+console.log(videoJuegos[0]);
+
+let arregloCosas = [
+    true,
+    123,
+    'Fernando',
+    1 + 2,
+    function(){},
+    ()=>{},
+    { a: 1 },
+    ['X', 'Megaman', 'Zero']
+];
+console.log(arregloCosas[7][2]);
