11611c9a4bc55c088f0ccbc7b38488b9a18eb4a4
Laura Gonzalez <laura.gonzalez@ias.com.co>
2023-03-01 19:12:43 -0500

    Programa

---
+++ Backend/ProgramacionFuncional/src/main/java/comprogramacion/programacionfuncional/ProgramacionFuncionalApplication.java
@@ -0,0 +1,13 @@
+package comprogramacion.programacionfuncional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgramacionFuncionalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProgramacionFuncionalApplication.class, args);
+    }
+
+}
---
+++ Backend/ProgramacionFuncional/src/test/java/comprogramacion/programacionfuncional/ProgramacionFuncionalApplicationTests.java
@@ -0,0 +1,13 @@
+package comprogramacion.programacionfuncional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgramacionFuncionalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
